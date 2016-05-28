a04a71dc7bac65868216fa0f40d6887169de7fd9
Martien Oppeneer <martien@oppeneer.net>
2016-05-28 23:36:59 +0200

    Added sqlite3 and some initial code to try it out.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "my_logger.h"
+#include <sqlite3.h>
 
 int main(int, char*[])
 {
@@ -15,6 +16,37 @@ int main(int, char*[])
     LOG(error)   << "An error severity message";
     LOG(fatal)   << "A fatal severity message";
 
+    sqlite3* db;
+
+    // open db
+    int retval = sqlite3_open("test.db", &db);
+    if (retval != 0)
+    {
+        LOG(fatal) << "Can't open sqlite3 db: " << sqlite3_errmsg(db);
+        sqlite3_close(db);
+        return 1;
+    }
+    else
+    {
+        LOG(info) << "Opened sqlite db 'test.db'";
+    }
+
+    // create a table
+    char* errmsg = 0;
+    retval = sqlite3_exec(db, "CREATE TABLE test(id PRIMARY KEY, col1, col2)", NULL, NULL, &errmsg);
+    if (retval != 0)
+    {
+        LOG(fatal) << "Error creating table: " << errmsg;
+        sqlite3_close(db);
+        return 2;
+    }
+    else
+    {
+        LOG(info) << "Created table 'test'";
+    }
+
+    sqlite3_close(db);
+    LOG(info) << "Closed sqlite db 'test.db'";
     return 0;
 }
 
