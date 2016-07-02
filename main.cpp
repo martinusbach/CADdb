cafdc7b683e9768d71950e2071d0c27207166f82
Martien Oppeneer <martien@oppeneer.net>
2016-07-02 21:52:48 +0200

    First elementary version of ObjReader: reads vertices and faces from a wavefront obj file, but then does nothing with it.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,85 @@
 #include "caddb.h"
 #include "my_logger.h"
+#include <string>
+#include <fstream>
+ 
+class ObjReader
+{
+public:
+    ObjReader(const char *obj_filename);
+    ~ObjReader();
+    void saveToDB(CadDB& db);
+private:
+    std::string m_obj_filename;
+};
+
+ObjReader::ObjReader(const char* obj_filename)
+    : m_obj_filename(obj_filename)
+{
+    ;
+}
+
+ObjReader::~ObjReader()
+{
+    ;
+}
+
+void ObjReader::saveToDB(CadDB &db)
+{
+    std::ifstream objFileStream(m_obj_filename, std::ifstream::in);
+    if (!objFileStream.is_open())
+    {
+        LOG(fatal) << "Could not open obj file...";
+        return; // TODO throw exception
+    }
+
+    const size_t max_chars = 256;
+    char line[max_chars];
+    char* token;
+    while(objFileStream.getline(line, max_chars))
+    {
+        token = strtok(line, " ");
+        if (token == NULL)
+        {
+            continue;
+        }
+        else if (*token == ' ' || *token == '#')
+        {
+            continue;
+        }
+
+        if (*token == 'v')
+        {
+            // start reading a vertex
+            double vertex[4];
+            int i = 0;
+            for (token = strtok(NULL, " ");
+                 token != NULL && i < 4;
+                 token = strtok(NULL, " "), ++i)
+            {
+                vertex[i] = strtod(token, NULL);
+            }
+            LOG(debug) << "vertex: " << vertex[0] << "|" << vertex[1] << "|"  << vertex[2] << "|"  << vertex[3] << "|";
+        }
+        else if (*token == 'g')
+        {
+            // start reading a group
+        }
+        else if (*token == 'f')
+        {
+            // start reading a face
+            int face_indices[3];
+            int i = 0;
+            for (token = strtok(NULL, " ");
+                 token != NULL && i < 3;
+                 token = strtok(NULL, " "), ++i)
+            {
+                face_indices[i] = strtol(token, NULL, 10);
+            }
+            LOG(debug) << "face indices: " << face_indices[0] << "|" << face_indices[1] << "|" << face_indices[2] << "|";
+        }
+    }
+}
 
 
 int main(int, char*[])
@@ -10,11 +90,12 @@ int main(int, char*[])
 
     try
     {
-        CadDB db("test.db");
-        CadDB db1("test1.db");
+        CadDB db("test.db");  // creates and opens an sqlite db
+        db.CreateCadTables(); // creates the basic tables
 
-        db.CreateTestTable();
-        db1.CreateCadTables();
+        ObjReader teapotReader("../sample_obj_files/teapot.obj");
+        teapotReader.saveToDB(db);
+
     }
     catch (CadDBException& exception)
     {
